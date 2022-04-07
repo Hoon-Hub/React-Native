c718a31cb6f3ec6b1f110013796597ae4b675aa8
Hoon-Hub <gwi01304@naver.com>
2022-04-07 18:00:54 +0900

    expo app todolist travel

---
+++ nomad/todoapp/App.js
@@ -0,0 +1,180 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { theme } from "./colors";
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const STORAGE_KEY = "@todos";
+
+export default function App() {
+  useEffect(() => {
+    loadTodo();
+  }, [todos]);
+
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState({});
+  const [nowPosition, setPosition] = useState(true);
+
+  const travel = async () => {
+    setWorking(false);
+    setPosition(false);
+    await AsyncStorage.setItem("tabs", nowPosition);
+  };
+  const work = async () => {
+    setWorking(true);
+    setPosition(true);
+    await AsyncStorage.setItem("tabs", nowPosition);
+  };
+
+  const saveTodos = async (toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+
+  const loadTodo = async () => {
+    try {
+      const s = await AsyncStorage.getItem(STORAGE_KEY);
+
+      if (!s) {
+        return;
+      } else {
+        setTodos(JSON.parse(s));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onChangeText = (payload) => {
+    setText(payload);
+  };
+
+  const addTodo = async () => {
+    if (text === "") return;
+    // save todo
+    const newTodos = { ...todos, [Date.now()]: { text, working } };
+
+    setTodos(newTodos);
+    await saveTodos(newTodos);
+    setText("");
+  };
+
+  const deleteTodo = async (key) => {
+    Alert.alert("Delete todo?", "Are you sure?", [
+      { text: "Cancel" },
+      {
+        text: "I'm sure",
+        onPress: () => {
+          try {
+            const newTodos = { ...todos };
+            delete newTodos[key];
+            setTodos(newTodos);
+            saveTodos(newTodos);
+            // await AsyncStorage.removeItem(key);
+          } catch (error) {
+            console.log(error);
+          }
+        },
+      },
+    ]);
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{
+              ...styles.buttonText,
+              color: working ? "white" : theme.grey,
+            }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.buttonText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          returnKeyType="done"
+          onSubmitEditing={addTodo}
+          onChangeText={onChangeText}
+          style={styles.input}
+          placeholder={working ? "Add a Todo" : "Where do you wanna go?"}
+        />
+      </View>
+      <ScrollView>
+        {Object.keys(todos).map((key) =>
+          todos[key].working === working ? (
+            <View style={styles.todo} key={key}>
+              <Text style={styles.todoText}>{todos[key].text}</Text>
+              <TouchableOpacity onPress={(key) => deleteTodo(key)}>
+                <Text>🍕</Text>
+              </TouchableOpacity>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  buttonText: {
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 10,
+    fontSize: 17,
+    textDecorationLine: "none",
+  },
+  todo: {
+    backgroundColor: theme.grey,
+    marginBottom: 7,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  todoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
