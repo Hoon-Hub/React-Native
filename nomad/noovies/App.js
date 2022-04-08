2b3d927427f43317a63712144db50cb3d2a94a0d
Hoon-Hub <gwi01304@naver.com>
2022-04-08 16:08:41 +0900

    preload

---
+++ nomad/noovies/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Text, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+export default function App() {
+  // preloading만 사용한다면 (다른 로직 처리 없이) useFonts, useAssets를 사용하는 것이 좋다.
+  const [fonts, fontsError] = Font.useFonts(Ionicons.font);
+  const [assets, assetsError] = useAssets([
+    require("./my-dog.png"),
+    "https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg",
+  ]);
+
+  // 다른 로직이 필요하다면
+  // AppLoading 로직을 사용하는것이 좋다.
+
+  if (!assets || !fonts) {
+    return <AppLoading />;
+  }
+  return <Text>Hello?</Text>;
+}
